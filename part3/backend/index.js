91accbf4f797188d8495a049f52c1947a93c3b19
EliasSchool <125263515+EliasSchool@users.noreply.github.com>
2023-03-11 21:30:20 +0200

    puhelinluettelo

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
+app.use(express.static('build')); 
 console.log("Hello World");
+
+
+app.use(morgan((tokens, req, res) => {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms', 
+        JSON.stringify(req.body),
+    ].join(' ');
+}))
 
 
 let persons = [
@@ -26,6 +43,15 @@ let persons = [
         id: 4
         }
     ]
+
+
+
+
+const generateID = () => {
+    const maxID = persons.length > 0 ? Math.max(...persons.map(p => p.id)) + 1 : 0 
+    return maxID 
+} 
+    
 
 app.get("/info", (req, res) => {
     const date  = new Date()
@@ -56,18 +82,37 @@ app.get("/api/persons", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
     const id = Number(req.params.id) 
     const person = persons.find(p => p.id === id)
-
+    const index = persons.findIndex(p => p.id === id)
+    if (index > -1) {
+        persons.splice(index, 1)
+    }
     res.status(204).end()
 })
 
 app.post("/api/persons", (req, res) => {
-    const person = req.body
-    console.log(person)
+    const body = req.body
+
+    if(!body.name ||!body.number) {
+        return res.status(400).json({ error: "Missing name or person" })
+    } 
+
+    const existingPerson = persons.some(person => person.name === body.name)
+    if (existingPerson) {
+        return res.status(400).json({ error: "Name already exists" })
+    }
+
+    const person = {
+        name : body.name,
+        number : body.number,
+        id: generateID() 
+    }
+
+    persons = persons.concat(person)
     res.json(person)
 });
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
