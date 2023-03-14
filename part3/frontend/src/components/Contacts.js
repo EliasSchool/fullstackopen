e7d17e2942119f47327629de71057eb6be7cb247
EliasSchool <125263515+EliasSchool@users.noreply.github.com>
2023-03-14 15:16:21 +0200

    Delete part3 directory

--- part3/frontend/src/components/Contacts.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Contacts = ({ persons, handleDelete}) => {
-    return (
-      <li key={persons.id}>{persons.name} {persons.number} <button onClick={() => handleDelete(persons.id, persons.name)}>Delete</button></li>
-    )
-  }
-
-export default Contacts
